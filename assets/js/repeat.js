1302de0474a62307e744339593b53bedc19eeacd
chihiro.tanigawa <chihiro.tanigawa@accenture.com>
2022-03-18 10:20:50 +0900

    ファイルUP

---
+++ assets/js/repeat.js
@@ -0,0 +1,26 @@
+
+
+const dat = new dat.GUI()
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+const wave = {
+  y: canvas.height / 2,
+  length: 0.01,
+  amplitude: 100
+}
+
+gui.add(wave, 'y' , 0, canvas.height)
+
+c.beginPath()
+
+c.moveTo(0, canvas.height / 2)
+
+for(let i = 0; i < canvas.width; i++) {
+  c.lineTo(i, canvas.height / 2 + Math.sin(i * 0.01) * 100)
+}
+
+c.stroke()
